db6ccdde485cedc2e8d61b6ae42067eab649591f
Jamie Smart <jamie.smart.10@gmail.com>
2026-03-31 10:59:48 -0400

    update list-page

--- src/app/list-page/list-page.ts
+++ src/app/list-page/list-page.ts
@@ -2,10 +2,12 @@ import { Component, computed, inject, resource } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { DBService } from '../services/DBService';
-
+import { TodoItem } from '../components/todo-item/todo-item';
+import { IconButton } from '../components/icon-button/icon-button';
 @Component({
   selector: 'list-page',
   templateUrl: './list-page.html',
+  imports: [TodoItem, IconButton],
 })
 export class ListPage {
   addItem() {
@@ -16,7 +18,7 @@ export class ListPage {
   private route = inject(ActivatedRoute);
   private data = toSignal(this.route.data);
   dbService = inject(DBService);
-  date = computed(() => (this.data() ? this.data()!['date'] : ''));
+  date = computed(() => (this.data() ? this.data()!['date'] : '12'));
 
   dbResource = resource({
     params: () => ({ date: this.date() }),
