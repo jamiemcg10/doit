096a6b809cb99699ce2472a4593ba356a5e9bb30
Jamie Smart <jamie.smart.10@gmail.com>
2026-04-02 12:06:07 -0400

    add calendar picker

--- src/app/list-page/list-page.ts
+++ src/app/list-page/list-page.ts
@@ -6,10 +6,16 @@ import { TodoItem } from '../components/todo-item/todo-item';
 import { IconButton } from '../components/icon-button/icon-button';
 import { Router } from '@angular/router';
 import { getNextDateString, getPrevDateString } from '../utils/getDateStrings';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'list-page',
   templateUrl: './list-page.html',
-  imports: [TodoItem, IconButton],
+  providers: [provideNativeDateAdapter()],
+  imports: [TodoItem, IconButton, MatDatepickerModule, MatInputModule, ReactiveFormsModule],
 })
 export class ListPage {
   private router = inject(Router);
@@ -19,15 +25,34 @@ export class ListPage {
   dbService = inject(DBService);
   date = computed(() => this.data()!['date']);
 
+  calendarDate = new FormControl(this.date());
+
   dbResource = resource({
     params: () => ({ date: this.date() }),
     loader: (params) => this.dbService.getItemsByDate(params.params.date),
   });
 
+  isLoading = computed(() => this.dbResource.status() === 'loading');
+  isError = computed(() => this.dbResource.status() === 'error');
+
   today = new Date().toLocaleDateString('en-CA');
+  dateString = computed(() =>
+    new Date(`${this.date()}T00:00`).toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'short',
+      day: 'numeric',
+      ...(new Date(`${this.date()}T00:00`).getFullYear() !== new Date().getFullYear() && {
+        year: 'numeric',
+      }),
+    }),
+  );
   items = linkedSignal(() => this.dbResource.value());
 
   adding = false;
+
+  handleDateChange() {
+    this.router.navigate([`/date/${this.calendarDate.value.toLocaleDateString('en-CA')}`]);
+  }
 
   reload() {
     this.dbResource.reload();
@@ -45,17 +70,13 @@ export class ListPage {
     this.adding = false;
 
     const prevDate = getPrevDateString(this.date());
-
     this.router.navigate([`/date/${prevDate}`]);
   }
 
   handleScrollForward() {
     this.adding = false;
-    const nextDate = getNextDateString(this.date());
 
+    const nextDate = getNextDateString(this.date());
     this.router.navigate([`/date/${nextDate}`]);
   }
-
-  isLoading = computed(() => this.dbResource.status() === 'loading');
-  isError = computed(() => this.dbResource.status() === 'error');
 }
